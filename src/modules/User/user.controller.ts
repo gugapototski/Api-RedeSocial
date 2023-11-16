17c24ee8bdaa2d646b1d55ce6fc023c7351afd7e
gugapototski <gugapototski@live.com>
2023-11-16 03:02:16 -0300

    Add da opção de alteração do user e sua delete do mesmo. Add busca por nome

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto';
 
@@ -9,8 +17,20 @@ export class UserController {
   async create(@Body() data: UserDTO) {
     return this.UserService.create(data);
   }
+  @Put('update/:id_user')
+  async update(@Param('id_user') id_user: string, @Body() data: UserDTO) {
+    return this.UserService.update(parseInt(id_user), data);
+  }
+  @Delete('delete/:id_user')
+  async delete(@Param('id_user') id_user: string) {
+    return this.UserService.Delete(parseInt(id_user));
+  }
   @Get('Allusers')
   async findAll() {
     return this.UserService.finfAll();
+  }
+  @Get('findbyname/:nome_apelido_user')
+  async findUserNome(@Param('nome_apelido_user') nome_apelido_user: string) {
+    return this.UserService.findUserNome(nome_apelido_user);
   }
 }
--- src/modules/User/user.service.ts
+++ src/modules/User/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { UserDTO } from './user.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
@@ -29,7 +30,64 @@ export class UserService {
       throw new NotFoundException('Senhas não correspondem');
     }
   }
+  async UserExists(id_user: number) {
+    const User = await this.prisma.user.findUnique({
+      where: {
+        id_user: id_user,
+      },
+    });
+    return User !== null;
+  }
+  async update(id_user: number, userData: UserDTO) {
+    const UserExists = await this.UserExists(id_user);
+
+    if (UserExists) {
+      const UpdateUser = await this.prisma.user.update({
+        data: {
+          nome_apelido_user: userData.nome_apelido_user,
+          email_user: userData.email_user,
+          senha_user: userData.senha_user,
+          confirmar_senha_user: userData.confirmar_senha_user,
+        },
+        where: {
+          id_user: id_user,
+        },
+      });
+      return UpdateUser;
+    } else {
+      throw new NotFoundException('Usuário não existe!');
+    }
+  }
+  async Delete(id_user: number) {
+    const UserExists = await this.UserExists(id_user);
+    if (UserExists) {
+      return await this.prisma.user.delete({
+        where: {
+          id_user: id_user,
+        },
+      });
+    } else {
+      throw new NotFoundException('Usuário não existe!');
+    }
+  }
   async finfAll() {
     return this.prisma.user.findMany();
+  }
+  async findUserNome(nome_apelido_user: string) {
+    const users = await this.prisma.user.findMany({
+      where: {
+        nome_apelido_user: {
+          contains: nome_apelido_user,
+          mode: 'insensitive', // Adicione esta linha para tornar a busca insensível a maiúsculas/minúsculas
+        },
+      },
+    });
+    if (users.length > 0) {
+      return users;
+    } else {
+      throw new NotFoundException(
+        'Nenhum usuário encontrado com o nome fornecido.',
+      );
+    }
   }
 }
